59a02332aa4a05f3da5210cc72f13fed0311e6fb
iAmUnableToSpell <98621396+iAmUnableToSpell@users.noreply.github.com>
2022-11-25 15:02:32 -0500

    Remove Outdated Backend

--- backend/config/Database.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
- 
-const db = new Sequelize('csds393_project', 'remote_admin', '1234', {
-    host: "ec2-52-7-212-92.compute-1.amazonaws.com",
-    dialect: "mysql"
-});
- 
-export default db;
