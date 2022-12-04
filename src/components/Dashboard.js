eb493d2a7e091a6f27c8ad3c42de8a220cc78255
Kerry <kjn45@case.edu>
2022-12-04 16:15:07 -0500

    Merge functionality

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,14 +3,20 @@ import React, { useState, useEffect } from 'react'
 import Navbar from "./Navbar";
 import { Auth, Amplify, Storage } from 'aws-amplify';
 import { Breadcrumb, Layout, Button, Modal, Space, Divider, Row, Col, Table, Tag } from 'antd';
-import { PDFDocument } from 'pdf-lib';
+import { ColorTypes, PDFDocument } from 'pdf-lib';
 const { Header, Footer, Sider, Content } = Layout;
 Storage.configure({ level: 'protected' });
 
 const Dashboard = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isPreviewOpen, setIsPreviewOpen] = useState(false);
+    const [isMergeOpen, setIsMergeOpen] = useState(false);
+    const [isSplitOpen, setIsSplitOpen] = useState(false);
     const [selectedFile, setSelectedFile] = useState(null);
+    const [mergeTargetKey, setMergeTargetKey] = useState(null);
+    const [firstPDF, setfirstPDF] = useState(null);
+    const [secondPDF, setsecondPDF] = useState(null);
+    const [resultPDF, setResultPDF] = useState(null);
     const [loaded, setLoaded] = useState(0);
     const [loadedKey, setLoadedKey] = useState(0);
     const [pdflist, setPdfList] = useState(new Array());
@@ -31,6 +37,14 @@ const Dashboard = () => {
         setIsPreviewOpen(false)
     }
 
+    const closeMerge = () => {
+        setIsMergeOpen(false)
+    }
+    
+    const closeSplit = () => {
+        setIsSplitOpen(false)
+    }
+
     const showPreview = () => {
         setIsPreviewOpen(true)
     }
@@ -43,12 +57,51 @@ const Dashboard = () => {
         setIsPreviewOpen(true)
     }
 
-    async function handleMerge(e) {
-        const result = await Storage.get(e, {download: true});
-        const url = URL.createObjectURL(result.Body);
+    async function handleEdit() {
+        const url = pdfString
         const arrayBuffer = await fetch(url).then(res => res.arrayBuffer());
-        const pdfDoc1 = await PDFDocument.load(arrayBuffer);
-        /* Add PDF filing */
+        const pdfDoc = await PDFDocument.load(arrayBuffer);
+        setfirstPDF(pdfDoc)
+        setIsMergeOpen(true)
+    }
+
+    async function handleMerge() {       
+        const mergedPdf = await PDFDocument.create();
+
+        /* Uses deafult for now- change to find table selection */
+        const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf' 
+        const arrayBuffer = await fetch(url).then(res => res.arrayBuffer());
+        const pdfDoc = await PDFDocument.load(arrayBuffer);
+        setsecondPDF(pdfDoc)
+
+        const copiedPagesA = await mergedPdf.copyPages(firstPDF, firstPDF.getPageIndices());
+        copiedPagesA.forEach((page) => mergedPdf.addPage(page));
+
+        const copiedPagesB = await mergedPdf.copyPages(secondPDF, secondPDF.getPageIndices());
+        copiedPagesB.forEach((page) => mergedPdf.addPage(page));
+
+        const mergedPdfFile = await mergedPdf.save();
+        setResultPDF(mergedPdfFile)
+
+        const file = resultPDF
+        file.name = loadedKey + "_merged"
+        let key = file.name
+        console.log("key: " + key)
+        try {
+            await Storage.put(file.name, file, {
+              contentType: "application/pdf", // contentType is optional
+            });
+          } catch (error) {
+            console.log("Error uploading file: ", error);
+          }
+        
+        setIsMergeOpen(false)
+        setIsPreviewOpen(false)
+        handleCancel()
+    }
+
+    async function handlePDFUpload(e) {
+
     }
 
     async function handleUpload(e) {
@@ -139,8 +192,47 @@ const Dashboard = () => {
             <Content style={{ padding: '0 50px' }}>
                 <Layout>
                     <Modal title="Document Preview" open={isPreviewOpen} onCancel={closePreview} footer={null} centered='true' width='1200'>
-                        <button type="primary" onClick={handleMerge}>Merge</button>
+                        <Col span={4}>
+                            <Button type="primary" onClick={async () => {await handleEdit();}}>Merge</Button>
+                        </Col>
+                        <Col span={4}>
+                            <Button type="primary" onClick={async () => {await handleEdit();}}>Split</Button>
+                        </Col>
+                        <br/>
+                        <br/>
                         <embed src={pdfString} width="1200" height="600"></embed>
+                    </Modal>
+                <Divider />
+                    <Modal title="Document to Merge" open={isMergeOpen} onCancel={closeMerge || closePreview} footer={null} centered='true' width='1200'>
+                    <Table columns={[
+                        {
+                            title: 'Name',
+                            dataIndex: 'name',
+                            key: 'name',
+                            render: (text) => <Button type="primary" onClick={async () => {await handleMerge();}}>{text}</Button>
+                        },
+                        {
+                            title: 'Last Edited:',
+                            dataIndex: 'lastedit',
+                            key: 'lastedit',
+                        }
+                    ]} dataSource={pdflist} />
+                    </Modal>
+                <Divider />
+                <Modal title="Document to Split" open={isSplitOpen} onCancel={closeMerge || closePreview} footer={null} centered='true' width='1200'>
+                    <Table columns={[
+                        {
+                            title: 'Name',
+                            dataIndex: 'name',
+                            key: 'name',
+                            render: (text) => <Button type="primary" onClick={async () => {await handleMerge();}}>{text}</Button>
+                        },
+                        {
+                            title: 'Last Edited:',
+                            dataIndex: 'lastedit',
+                            key: 'lastedit',
+                        }
+                    ]} dataSource={pdflist} />
                     </Modal>
                 <Divider />
                 <div className="site-layout-content">
