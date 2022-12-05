fb6888302d811e382c3e425fa2d690a0e66d9eb4
Kerry <kjn45@case.edu>
2022-12-04 20:50:52 -0500

    Merge and Split functionality
    Merge needs to be changed and updated with view

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,6 +4,7 @@ import Navbar from "./Navbar";
 import { Auth, Amplify, Storage } from 'aws-amplify';
 import { Breadcrumb, Layout, Button, Modal, Space, Divider, Row, Col, Table, Tag } from 'antd';
 import { ColorTypes, PDFDocument } from 'pdf-lib';
+import Input from 'antd/lib/input/Input';
 const { Header, Footer, Sider, Content } = Layout;
 Storage.configure({ level: 'protected' });
 
@@ -12,11 +13,14 @@ const Dashboard = () => {
     const [isPreviewOpen, setIsPreviewOpen] = useState(false);
     const [isMergeOpen, setIsMergeOpen] = useState(false);
     const [isSplitOpen, setIsSplitOpen] = useState(false);
+    const [hasInput, setHasInput] = useState(false);
+    const [splitInput, setSplitInput] = useState(null);
     const [selectedFile, setSelectedFile] = useState(null);
     const [mergeTargetKey, setMergeTargetKey] = useState(null);
     const [firstPDF, setfirstPDF] = useState(null);
     const [secondPDF, setsecondPDF] = useState(null);
     const [resultPDF, setResultPDF] = useState(null);
+    const [resultPDFTwo, setResultPDFTwo] = useState(null);
     const [loaded, setLoaded] = useState(0);
     const [loadedKey, setLoadedKey] = useState(0);
     const [pdflist, setPdfList] = useState(new Array());
@@ -30,6 +34,7 @@ const Dashboard = () => {
     const handleCancel = () => {
         setSelectedFile(null);
         setIsModalOpen(false);
+        setSplitInput(null);
         getList();
     };
 
@@ -43,11 +48,17 @@ const Dashboard = () => {
     
     const closeSplit = () => {
         setIsSplitOpen(false)
+        setSplitInput(null);
     }
 
     const showPreview = () => {
         setIsPreviewOpen(true)
     }
+
+    const handleChange = event => {
+        setSplitInput(event.target.value);
+        setHasInput(true)
+      };
     
     async function handleDownload(e) {
         const result = await Storage.get(e, {download: true});
@@ -57,12 +68,15 @@ const Dashboard = () => {
         setIsPreviewOpen(true)
     }
 
-    async function handleEdit() {
+    async function handleEdit(isMerge) {
         const url = pdfString
         const arrayBuffer = await fetch(url).then(res => res.arrayBuffer());
         const pdfDoc = await PDFDocument.load(arrayBuffer);
         setfirstPDF(pdfDoc)
-        setIsMergeOpen(true)
+        if(isMerge)
+            setIsMergeOpen(true)
+        else
+            setIsSplitOpen(true)
     }
 
     async function handleMerge() {       
@@ -94,14 +108,60 @@ const Dashboard = () => {
           } catch (error) {
             console.log("Error uploading file: ", error);
           }
-        
         setIsMergeOpen(false)
         setIsPreviewOpen(false)
         handleCancel()
     }
 
-    async function handlePDFUpload(e) {
+    async function handleSplit(){
+        //console.log(e)
+        const subDocument1 = await PDFDocument.create();
+        const subDocument2 = await PDFDocument.create();
+        const totalPages = firstPDF.getPageCount();
+        if(splitInput >= totalPages || splitInput <= 0){
+            setIsSplitOpen(false)
+            setIsPreviewOpen(false)
+            handleCancel()
+            return;
+        }
+        else{
+            for(let i = 0; i < splitInput; i++){
+                const [partedPage] = await subDocument1.copyPages(firstPDF, [i])
+                subDocument1.addPage(partedPage)
+            }
 
+            for(let i = splitInput; i < totalPages; i++){
+                const [partedPage2] = await subDocument2.copyPages(firstPDF, [i])
+                subDocument2.addPage(partedPage2)
+            }
+            const mergedPDFHalf = await subDocument1.save();
+            const mergedPDFHalf2 = await subDocument2.save();
+            setResultPDF(mergedPDFHalf)
+            setResultPDFTwo(mergedPDFHalf2)
+
+            const file = resultPDF
+            file.name = loadedKey + "_split1"
+            let key = file.name  
+            const file2 = resultPDFTwo
+            file2.name = loadedKey + "_split2"
+            let key2 = file2.name
+            console.log("key: " + key)
+            console.log("key: " + key2)
+            try {
+                await Storage.put(file.name, file, {
+                contentType: "application/pdf", // contentType is optional
+                });
+                await Storage.put(file2.name, file2, {
+                    contentType: "application/pdf", // contentType is optional
+                    });
+            } catch (error) {
+                console.log("Error uploading file: ", error);
+            }
+            
+            setIsSplitOpen(false)
+            setIsPreviewOpen(false)
+            handleCancel()
+        }
     }
 
     async function handleUpload(e) {
@@ -193,7 +253,7 @@ const Dashboard = () => {
                 <Layout>
                     <Modal title="Document Preview" open={isPreviewOpen} onCancel={closePreview} footer={null} centered='true' width='1200'>
                         <Col span={4}>
-                            <Button type="primary" onClick={async () => {await handleEdit();}}>Merge</Button>
+                            <Button type="primary" onClick={async () => {await handleEdit(true);}}>Merge</Button>
                         </Col>
                         <Col span={4}>
                             <Button type="primary" onClick={async () => {await handleEdit();}}>Split</Button>
@@ -219,21 +279,12 @@ const Dashboard = () => {
                     ]} dataSource={pdflist} />
                     </Modal>
                 <Divider />
-                <Modal title="Document to Split" open={isSplitOpen} onCancel={closeMerge || closePreview} footer={null} centered='true' width='1200'>
-                    <Table columns={[
-                        {
-                            title: 'Name',
-                            dataIndex: 'name',
-                            key: 'name',
-                            render: (text) => <Button type="primary" onClick={async () => {await handleMerge();}}>{text}</Button>
-                        },
-                        {
-                            title: 'Last Edited:',
-                            dataIndex: 'lastedit',
-                            key: 'lastedit',
-                        }
-                    ]} dataSource={pdflist} />
-                    </Modal>
+                <Modal title="Split after page..." open={isSplitOpen} onCancel={closeSplit || closePreview} footer={null} centered='true' width='1200'>
+                   <Input type="number" onChange={handleChange} value={splitInput} placeholder='Insert Page Number'></Input>
+                    <br/>
+                    <br/>
+                   <Button type="primary" disabled={!hasInput} onClick={async () => {await handleSplit();}}>Confirm</Button>
+                </Modal>
                 <Divider />
                 <div className="site-layout-content">
                     <Table columns={[
